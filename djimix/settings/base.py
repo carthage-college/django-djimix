2f102bb8e7dcd5f6020e69e39f992f71ef6a7143
plungerman <plungerman@gmail.com>
2019-09-09 07:45:14 -0500

    removed unused settings

--- djimix/settings/base.py
+++ djimix/settings/base.py
@@ -196,9 +196,6 @@ EMAIL_FAIL_SILENTLY = False
 DEFAULT_FROM_EMAIL = ''
 SERVER_EMAIL = ''
 SERVER_MAIL=''
-# app settings
-CSS_GROUP='CenterForStudentSuccess'
-CSS_EMAIL=''
 REQUIRED_ATTRIBUTE = True
 # logging
 LOG_FILEPATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs/')
