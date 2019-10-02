6919627f91ec458203852333467989d5798c0fda
plungerman <plungerman@gmail.com>
2019-10-02 09:36:14 -0500

    updated sql incantation so that student information for djsapo is always returned

--- djimix/sql/students.py
+++ djimix/sql/students.py
@@ -16,14 +16,14 @@ SELECT
             'Resident'
         END
     AS
-        residency_status,
+        residency_status,stu_serv_rec.stusv_no,
     stu_serv_rec.yr, stu_serv_rec.sess, stu_serv_rec.bldg, stu_serv_rec.room,
-    id_rec.lastname, id_rec.firstname, id_rec.id, id_rec.ss_no,
-    id_rec.addr_line1, id_rec.addr_line2, id_rec.city, id_rec.st,
+    stu_serv_rec.add_date, id_rec.lastname, id_rec.firstname, id_rec.id,
+    id_rec.ss_no, id_rec.addr_line1, id_rec.addr_line2, id_rec.city, id_rec.st,
     id_rec.zip, id_rec.ctry, id_rec.phone, cvid_rec.ldap_name,
     adm_rec.plan_enr_sess,adm_rec.plan_enr_yr,
     profile_rec.birth_date, profile_rec.sex, mobile_rec.phone as mobile,
-    stu_acad_rec.sess, prog_enr_rec.cl,
+    prog_enr_rec.cl,
     prog_enr_rec.adv_id, prog_enr_rec.subprog, prog_enr_rec.lv_date,
         TRIM(
             CASE
@@ -62,8 +62,6 @@ LEFT JOIN
 LEFT JOIN
     conc_table conc2    ON  prog_enr_rec.conc2     = conc2.conc
 LEFT JOIN
-    stu_acad_rec    ON  id_rec.id   =   stu_acad_rec.id
-LEFT JOIN
     adm_rec     ON  id_rec.id = adm_rec.id
 LEFT JOIN
     cvid_rec     ON  id_rec.id = cvid_rec.cx_id
@@ -78,33 +76,7 @@ LEFT JOIN
     (id_rec.id = mobile_rec.id AND mobile_rec.aa = "ENS")
 WHERE
     id_rec.id = {cid}
-AND
-    cc_student_medical_manager.created_at > MDY(
-        6, 1,
-        CASE
-          WHEN
-            month(CURRENT) < 6
-          THEN
-            YEAR(TODAY- 1 UNITS YEAR)
-          ELSE
-            YEAR(TODAY)
-        END
-    )
-AND
-    stu_serv_rec.yr = year(CURRENT)
-AND
-  stu_serv_rec.sess =
-  CASE
-    WHEN month(CURRENT) > 6 THEN "RA"
-    ELSE "RC"
-  END
-and
-  stu_acad_rec.SESS =
-  CASE
-    WHEN month(CURRENT) > 6 THEN "RA"
-    ELSE "RC"
-  END
-ORDER BY stu_serv_rec.yr DESC
+ORDER BY stu_serv_rec.add_date DESC
 '''.format
 
 ADMISSIONS_REP = '''
