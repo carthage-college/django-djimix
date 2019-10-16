113fb84f7e43a6065da3b81f90cf5fa524b78ff7
plungerman <plungerman@gmail.com>
2019-10-16 09:08:46 -0500

    struggling with database encoding, again. stupid informix

--- djimix/core/database.py
+++ djimix/core/database.py
@@ -32,24 +32,35 @@ def get_connection(earl=None, encoding=True):
             #cnxn.setencoding(encoding='utf8', ctype=pyodbc.SQL_CHAR)
             #cnxn.setencoding(encoding='latin1', ctype=pyodbc.SQL_CHAR)
             #cnxn.setencoding(encoding='cp1252', ctype=pyodbc.SQL_CHAR)
+            cnxn.setencoding(encoding='utf-32le', ctype=pyodbc.SQL_CHAR)
 
+            # data
             #cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
             #cnxn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
-            cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='cp1252')
-            cnxn.setdecoding(pyodbc.SQL_CHAR, encoding='cp1252')
+            #cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='cp1252')
+            #cnxn.setdecoding(pyodbc.SQL_CHAR, encoding='cp1252')
+            #cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='latin1')
+            #cnxn.setdecoding(pyodbc.SQL_CHAR, encoding='latin1')
+            cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-32le')
+            cnxn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-32le')
 
-            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='cp1252')
-            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-16')
-            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-16le')
-            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-16be')
-            cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-32le')
+            # field names
+            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='latin1', ctype=pyodbc.SQL_CHAR)
+            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='cp1252', ctype=pyodbc.SQL_CHAR)
+            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-16', ctype=pyodbc.SQL_CHAR)
+            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-16le', ctype=pyodbc.SQL_CHAR)
+            #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='latin1', ctype=pyodbc.SQL_CHAR)
             #cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-8', ctype=pyodbc.SQL_CHAR)
+
+            # current and working
+            cnxn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-32le', ctype=pyodbc.SQL_CHAR)
+
+
 
             #utf32b
 
-            #cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='latin1')
-            #cnxn.setdecoding(pyodbc.SQL_CHAR, encoding='latin1')
-            cnxn.setencoding(encoding='utf-8')
+            cnxn.setencoding(encoding='utf-32le')
+            #cnxn.setencoding(encoding='utf-8')
             #cnxn.setencoding(encoding='latin1')
             #cnxn.setencoding(encoding='cp1252')
         except:
