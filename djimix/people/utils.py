275da15945ea2add4ad055b40ab4f76739e4983a
plungerman <plungerman@gmail.com>
2020-01-15 14:23:41 -0600

    moved get_peeps() from djsapo to here so that we can share it with other projects like djbeca

--- djimix/people/utils.py
+++ djimix/people/utils.py
@@ -34,3 +34,44 @@ def get_position(tpos):
             results = settings.TPOS_DEFAULT[tpos]
         cache.set(key, results, None)
     return results
+
+
+def get_peeps(who):
+    key = 'provisioning_vw_{}_api'.format(who)
+    peeps = cache.get(key)
+
+    if peeps is None:
+
+        if who == 'facstaff':
+            where = 'faculty IS NOT NULL OR staff IS NOT NULL'
+        elif who in ['faculty','staff','student']:
+            where = '{} IS NOT NULL'.format(who)
+        else:
+            where = None
+
+    if not peeps and where:
+        sql = """
+            SELECT
+                id, lastname, firstname, username
+            FROM
+                provisioning_vw
+            WHERE
+                {}
+            ORDER BY
+                lastname, firstname
+        """.format(where)
+
+        objects = xsql(sql)
+
+        if objects:
+            peeps = []
+            for obj in objects:
+                row = {
+                    'cid': obj[0],
+                    'lastname': obj[1], 'firstname': obj[2],
+                    'email': '{}@carthage.edu'.format(obj[3])
+                }
+                peeps.append(row)
+            cache.set(key, peeps, timeout=86400)
+
+    return peeps
