44030994ec4778c47baa485fdfbae384f5dff432
plungerman <plungerman@gmail.com>
2020-08-04 14:30:49 -0500

    new function to obtain the UUID based on email address

--- djimix/core/utils.py
+++ djimix/core/utils.py
@@ -1,10 +1,24 @@
 from django.conf import settings
+from django.core.validators import validate_email
 from djimix.core.database import get_connection, xsql
 
 import re
 
 
+def get_uuid(email):
+    """Obtains the UUID based on the provided email address."""
+    row = None
+    if validate_email(email):
+        sql = "SELECT * FROM fwk_user WHERE email='{}'".format(email)
+        connection = get_connection(settings.MSSQL_EARL, encoding=False)
+        with connection:
+            results = xsql(sql, connection)
+            row = results.fetchone()
+    return row
+
+
 def get_userid(jenzabarUserID):
+    """Obtains the user ID based on the UUID provided."""
     # prevent folks from submitting anything but the fwk_user id
     #reggie="([\w\-\.]+[\-\.][\w\-\.]+)(\d+)\-(\w+)"
     #reggie="(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})"
