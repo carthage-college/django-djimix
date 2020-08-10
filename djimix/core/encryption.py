86da15c5c9cca6e7b19b331d734ed67dfecfd8b0
plungerman <plungerman@gmail.com>
2020-08-09 21:46:37 -0500

    using b32 instead b64 for now

--- djimix/core/encryption.py
+++ djimix/core/encryption.py
@@ -11,10 +11,11 @@ def encrypt(txt):
         txt = str(txt)
         # key should be byte
         cipher_suite = Fernet(settings.ENCRYPTION_KEY)
-        # #input should be byte, so convert the text to byte
+        # input should be byte, so convert the text to byte
         encrypted_text = cipher_suite.encrypt(txt.encode('ascii'))
         # encode to urlsafe base64 format
-        encrypted_text = base64.urlsafe_b64encode(encrypted_text).decode("ascii")
+        #encrypted_text = base64.urlsafe_b64encode(encrypted_text).decode('ascii')
+        encrypted_text = base64.b32encode(encrypted_text).decode('ascii')
         return encrypted_text
     except Exception as e:
         # log the error if any
@@ -25,9 +26,10 @@ def encrypt(txt):
 def decrypt(txt):
     try:
         # base64 decode
-        txt = base64.urlsafe_b64decode(txt)
+        # txt = base64.urlsafe_b64decode(txt)
+        txt = base64.b32decode(txt)
         cipher_suite = Fernet(settings.ENCRYPTION_KEY)
-        decoded_text = cipher_suite.decrypt(txt).decode("ascii")
+        decoded_text = cipher_suite.decrypt(txt).decode('ascii')
         return decoded_text
     except Exception as e:
         # log the error
